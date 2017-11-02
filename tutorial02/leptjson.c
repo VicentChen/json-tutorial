631d11cadf729d6302dc850d778d1bf6096a20c5
Vicent_Chen <1336754721@qq.com>
2017-11-02 22:36:11 +0800

    完成数字解析及其单元测试

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>  /* NULL, strtod() */
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 
 typedef struct {
     const char* json;
@@ -21,8 +23,7 @@ static void lept_parse_whitespace(lept_context* c) {
 */
 static int lept_parse_literal(lept_context* c, lept_value* v, const char* s, lept_type t)
 {
-	for (*s; *s != 0; s++)
-	{
+	for (*s; *s != 0; s++) {
 		if (*c->json != *s)
 			return LEPT_PARSE_INVALID_VALUE;
 		(c->json)++;
@@ -32,14 +33,37 @@ static int lept_parse_literal(lept_context* c, lept_value* v, const char* s, lep
 }
 
 static int lept_parse_number(lept_context* c, lept_value* v) {
-    char* end;
+    char* end = c->json;
     /* \TODO validate number */
-    v->n = strtod(c->json, &end);
-    if (c->json == end)
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json = end;
-    v->type = LEPT_NUMBER;
-    return LEPT_PARSE_OK;
+	/* phase 1 */
+	if (*end == '-') end++;
+	if (*end == '0')
+		end++;
+	else if (ISDIGIT1TO9(*end))
+		for (*end; ISDIGIT(*end); end++);
+	else
+		return LEPT_PARSE_INVALID_VALUE;
+
+	/* phase 2 */
+	if (*end == '.') {
+		end++;
+		if (!ISDIGIT(*end)) return LEPT_PARSE_INVALID_VALUE;
+		for (*end; ISDIGIT(*end); end++);
+	}	
+
+	/* phase 3 */
+	if (*end == 'e' || *end == 'E') {
+		end++;
+		if (*end == '+' || *end == '-')
+			end++;
+		if (!ISDIGIT(*end)) return LEPT_PARSE_INVALID_VALUE;
+		for (*end; ISDIGIT(*end); end++);
+	}
+
+	v->n = strtod(c->json, NULL);
+	c->json = end;
+	v->type = LEPT_NUMBER;
+	return LEPT_PARSE_OK;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
